d3e7a4609478a12c4c1659a9e0bcd105db498906
Patricia Nykaza <pnykaza@calstatela.edu>
2018-08-23 17:45:29 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,13 +19,13 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT name, MAX(age) FROM bears"
+  # "SELECT name, MAX(age) FROM bears"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "Write your SQL query here"
+  "SELECT name, MAX(age) FROM bears"
 end
 
 def selects_bear_that_killed_Tim
-  "Write your SQL query here"
+  # "SELECT name, MAX(age) FROM bears"
 end
